0f308908225746aed93a136d86e2b79b335f485c
Jalen <jalenms2012@gmail.com>
2023-11-19 19:14:06 -0800

    Wrote psuedo code game logic. Added images folder.

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Blackjack
-Fully function blackjack game with wagering implemented
+Fully functional blackjack game with betting implemented
+
+# Title
+Will be welcomed by a start screen
+Fade into the game room
+
+# Game
+The game of blackjack is simple
+Play against the dealer and whoever has the highest hand wins, but hand cannot be higher than 21
+If dealer goes over 21, player wins
+If player goes over 21, dealer wins
+If hand is = to 21, player or dealer gets BLACKJACK
+Starting Ace count for both hands is 0
+
+
+# Betting
+Player gets 100 dollars to start the game
+Player has ability to bet 20 or more dollars, if player goes below 20, Game is over.
+
+# Game Over screen
+If you lose enough money and can't bet anymore, welcomed by a game over screen.
+
---
+++ script.js
@@ -0,0 +1,34 @@
+//starting sum of hands
+let dealerNumber = 0;
+let playerNumber = 0;
+
+//deck and hidden card declarations
+let deck;
+let hiddenCard;
+
+//ace count
+let dealerAceCount = 0;
+let playerAceCount = 0;
+
+//FUNCTIONS FOR THE GAME
+//build a function to create a deck of cards
+
+//build a function to shuffle the deck so we dont get the same cards
+
+//build a function to deal cards to the player and dealer on load, dealer has one card hidden
+
+//build a function for the hit button, which will deal a card to the player. if the player busts, the dealer wins. if the player hits 21, the player wins. 
+
+//if the player gets an ace the player the function
+
+//build a function for the stand button, which will reveal the hidden card and deal cards to the dealer until they reach 17 or bust
+
+
+
+
+
+
+
+
+
+
