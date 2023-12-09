22debef60288ae17dad2a00440afec4dd3a4204e
Jalen <jalenms2012@gmail.com>
2023-12-08 23:08:32 -0800

    comments added, more updates to ui

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ const hiddenCardImg = document.getElementById('hidden-card')
 //buttons and descriptions
 const hitButton = document.getElementById('hit-button')
 const standButton = document.getElementById('stand-button')
+const doubleDownButton = document.getElementById('dd-button')
 const betButton = document.getElementById('bet-button')
 const betAmountInput = document.getElementById('bet-input-field')
 const betError = document.getElementById('bet-error')
@@ -45,11 +46,11 @@ const betDescription = document.getElementById('bet-description')
 const gameLog = document.getElementById('game-log-text')
 
 //disable buttons
-hitButton.style.display = 'none'
-standButton.style.display = 'none'
 playAgainButton.style.display = 'none'
 
 //FUNCTIONS FOR THE GAME
+
+//creating an array of cards
 function createDeck() {
   let values = [
     'A',
@@ -89,7 +90,7 @@ shuffleDeck();
 playerBalance.textContent = '$' + playerMoney;
 betAmountTitle.style.display = 'none';
 
-//moving the title and betting container up
+//moving the title and betting container up when the player bets
 function moveDescription() {
   bettingContainer.style.marginTop = '2rem';
   bettingContainer.style.animation = 'fadeIn 1s ease-in-out';
@@ -103,6 +104,7 @@ function playerBet() {
   betAmount = betAmountInput.value;
   betDescription.textContent = null;
 
+  //checking for betting errors
   if (betAmount < minBet) {
     betError.textContent = 'Minimum bet is $' + minBet;
     betAmountInput.value = null;
@@ -123,17 +125,16 @@ function playerBet() {
     betButton.style.display = 'none';
     hitButton.style.display = '';
     standButton.style.display = '';
+    doubleDownButton.style.display = '';
     heroContainer.style.display = 'flex';
   }
 }
 
-betButton.addEventListener('click', playerBet);
-
 //grabbing the card number from the deck array by using regular expressions to remove the suit from the array
 function getCardNumber(card, total) {
-  let value = card.match(/\d+|[A-Z]/)[0]; // remove the last character using regular expression
+  let value = card.match(/\d+|[A-Z]/)[0]; // remove the last character using regular expression 
   if (value === 'A') {
-    return total + 11 <= 21 ? 11 : 1;
+    return total + 11 <= 21 ? 11 : 1; //if the total is less than or equal to 21, return 11, otherwise return 1
   } else if (['K', 'Q', 'J'].includes(value)) {
     return 10;
   } else {
@@ -143,10 +144,12 @@ function getCardNumber(card, total) {
 
 // dealing the cards when the player bets
 function dealCards() {
+  //popping the cards from the deck array
   const dealerCard1 = deck.pop();
   const playerCard1 = deck.pop();
   const playerCard2 = deck.pop();
 
+  //creating the card images and adding them to the dealer and player's hand
   const dealerCard1Img = document.createElement('img');
   dealerCard1Img.classList.add('dealer-card');
   const playerCard1Img = document.createElement('img');
@@ -154,6 +157,7 @@ function dealCards() {
   const playerCard2Img = document.createElement('img');
   playerCard2Img.classList.add('player-card-two');
 
+  //pushing the '.png' to the end of the card name to get the image
   dealerCard1Img.src = `card-imgs/${dealerCard1}.png`;
   playerCard1Img.src = `card-imgs/${playerCard1}.png`;
   playerCard2Img.src = `card-imgs/${playerCard2}.png`;
@@ -162,10 +166,18 @@ function dealCards() {
   document.getElementById('player-hand').appendChild(playerCard1Img);
   document.getElementById('player-hand').appendChild(playerCard2Img);
 
+  //adding the card hand number to the dealer and player's hand
   dealerNumber += getCardNumber(dealerCard1, dealerNumber);
   playerNumber += getCardNumber(playerCard1, playerNumber);
   playerNumber += getCardNumber(playerCard2, playerNumber);
 
+  //checking for blackjack and disabling buttons if the player has blackjack
+  if (playerNumber === 21) {
+    doubleDownButton.disabled = true;
+    hitButton.disabled = true;
+  }
+
+  //delaying the dealer and player's hand number to show up
   setTimeout(function () {
     dealerHandNumber.textContent = ' ' + dealerNumber;
     playerHandNumber.textContent = ' ' + playerNumber;
@@ -185,18 +197,21 @@ function playerHit() {
   document.getElementById('player-hand').appendChild(newCardImg);
   playerNumber += getCardNumber(newCard, playerNumber);
   playerHandNumber.textContent = ' ' + playerNumber;
+  doubleDownButton.style.display = 'none';
+  betError.textContent = '';
 
+  //checking for blackjack and disabling buttons if the player has blackjack, drawing cards if true
   if (playerNumber > 21) {
     checkWinner();
   } else if (playerNumber === 21) {
+    showHiddenCard();
     setTimeout(dealerDraw, 2000);
-    gameLog.textContent = 'You got Blackjack!'; //fix the hidden card being shown when the player gets blackjack before presentation
+    gameLog.textContent = 'You got Blackjack!'; 
     hitButton.disabled = true;
     standButton.disabled = true;
+    doubleDownButton.disabled = true;
   }
 }
-
-hitButton.addEventListener('click', playerHit);
 
 //dealer draw functionality, creates a new card and adds it to the dealer's hand every two seconds until the dealers hand is greater than 17
 function dealerDraw() {
@@ -208,6 +223,34 @@ function dealerDraw() {
   dealerNumber += getCardNumber(newCard, dealerNumber);
   dealerHandNumber.textContent = ' ' + dealerNumber;
 
+  
+  if (dealerNumber < 17) {
+    setTimeout(dealerDraw, 2000);
+} else if (dealerNumber === 21) {
+    checkWinner();
+} else {
+    checkWinner();
+}
+}
+
+//show hidden card functionality, shows the hidden card and adds it to the dealer's hand
+function showHiddenCard() {
+    const hiddenCard = deck.pop();
+    hiddenCardImg.src = `card-imgs/${hiddenCard}.png`;
+    dealerNumber += getCardNumber(hiddenCard, dealerNumber);
+    dealerHandNumber.textContent = ' ' + dealerNumber;
+}
+
+//stand functionality, player cannot hit after this and allows the dealer to draw cards
+function stand() {
+  hitButton.disabled = true;
+  doubleDownButton.disabled = true;
+
+  showHiddenCard();
+  dealerHandNumber.textContent = ' ' + dealerNumber;
+  standButton.disabled = true;
+  betError.textContent = '';
+
   if (dealerNumber < 17) {
     setTimeout(dealerDraw, 2000);
   } else {
@@ -215,24 +258,29 @@ function dealerDraw() {
   }
 }
 
-//stand functionality, player cannot hit after this and allows the dealer to draw cards
-function stand() {
-  hitButton.disabled = true;
+//double down functionality, doubles the bet amount and adds a card to the player's hand if the player has enough money to do so
+function doubleDown() {
+    if (playerMoney < betAmount) {
+        betError.textContent = 'You do not have enough money to double down.';
+        return;
+    } 
 
-  const hiddenCard = deck.pop();
-  hiddenCardImg.src = `card-imgs/${hiddenCard}.png`;
-  dealerNumber += getCardNumber(hiddenCard, dealerNumber);
-  dealerHandNumber.textContent = ' ' + dealerNumber;
-  standButton.disabled = true;
+    //doubling the bet amount and adding a card to the player's hand because it counts as a hit
+    playerMoney -= betAmount;
+    playerBalance.textContent = '$' + playerMoney;
+    betAmount *= 2;
+    betNumber.textContent = '$' + betAmount;
+    doubleDownButton.disabled = true;
 
-  if (dealerNumber < 17) {
-    setTimeout(dealerDraw, 2000); //using recursion
-  } else {
-    checkWinner();
-  }
+    playerHit();
+
+    //delaying the dealer's draw and nullifying the function if the player busts
+    if (playerNumber > 21) {
+        checkWinner();
+    } else {
+        setTimeout(stand, 2000);
+    }
 }
-
-standButton.addEventListener('click', stand);
 
 //play again functionality, resets the game to original state
 function playAgain() {
@@ -250,12 +298,13 @@ function playAgain() {
   //resetting it back with styles
   hiddenCardImg.src = 'card-imgs/backcard.png';
   hiddenCardImg.style.animation = 'none';
-  hiddenCardImg.offsetHeight; //reflow
+  hiddenCardImg.offsetHeight; //reflow trick
   hiddenCardImg.style.animation = 'moveInLeft 2s ease-in-out 1s backwards';
   hiddenCardImg.style.animationFillMode = 'backwards';
   hiddenCardImg.style.animationDelay = '1s';
   hiddenCardImg.style.opacity = '1';
 
+  //resetting the game
   dealerHandNumber.textContent = ' ';
   playerHandNumber.textContent = ' ';
 
@@ -265,15 +314,8 @@ function playAgain() {
 
   dealerHandNumber.style.animation = 'none';
   playerHandNumber.style.animation = 'none';
-  dealerHandNumber.offsetHeight; // reflow
-  playerHandNumber.offsetHeight; // reflow
-
-  setTimeout(function () {
-    dealerHandNumber.textContent = ' ' + dealerNumber;
-    playerHandNumber.textContent = ' ' + playerNumber;
-    dealerHandNumber.style.animation = 'fadeIn 1s ease-in-out';
-    playerHandNumber.style.animation = 'fadeIn 1s ease-in-out';
-  }, 3500);
+  dealerHandNumber.offsetHeight; // reflow trick
+  playerHandNumber.offsetHeight; // reflow trick
 
   deck = [];
   createDeck();
@@ -290,7 +332,9 @@ function playAgain() {
 
   gameLog.textContent = null;
 
+  betDescription.textContent = 'Enter a bet amount to start the next round!';
   betNumber.textContent = null;
+  betError.textContent = '';
 
   dealCards();
 }
@@ -301,7 +345,11 @@ function checkWinner() {
   hitButton.disabled = false;
   standButton.disabled = false;
   standButton.style.display = 'none';
+  doubleDownButton.style.display = 'none';
+  doubleDownButton.disabled = false;
   playAgainButton.style.display = '';
+
+  //different scenarios for who wins
   if (playerNumber > 21) {
     gameLog.textContent = 'You busted! Dealer wins.';
   } else if (dealerNumber > 21) {
@@ -316,7 +364,8 @@ function checkWinner() {
     gameLog.textContent = 'Push!';
     playerMoney += parseInt(betAmount, 10);
   }
-
+  
+  //reload the page if the player's money is less than 20 on click
   if (playerMoney < 20) {
     gameLog.textContent = 'Game over.';
     playAgainButton.addEventListener('click', function () {
@@ -325,6 +374,11 @@ function checkWinner() {
   }
 
   playerBalance.textContent = '$' + playerMoney;
-
-  playAgainButton.addEventListener('click', playAgain);
 }
+
+//event listeners
+hitButton.addEventListener('click', playerHit);
+standButton.addEventListener('click', stand);
+doubleDownButton.addEventListener('click', doubleDown);
+betButton.addEventListener('click', playerBet);
+playAgainButton.addEventListener('click', playAgain);
